ac9aba622c519398ba44345249bb5f3e19e05c72
jonathan small <jonathn6@icloud.com>
2026-04-10 14:28:16 -0400

    Update server.js - increase .pdf file size
    2025_04_10 14:28:00

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 // ✅ iCloud SMTP transporter
 const transporter = nodemailer.createTransport({
