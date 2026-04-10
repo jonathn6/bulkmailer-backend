dacdf5a0213df99dcf860cb2a6da7499ee6beccb
jonathan small <jonathn6@icloud.com>
2026-04-10 14:50:51 -0400

    Update server.js
    2025_04_10 14:50:00

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import nodemailer from "nodemailer";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
@@ -28,6 +27,9 @@ transporter.verify(function (error, success) {
 });
 
 app.post("/send", async (req, res) => {
+  console.log("File name:", req.body.fileName);
+console.log("File data exists:", !!req.body.fileData);
+console.log("File data length:", req.body.fileData?.length);
   console.log("Incoming request:", req.body);
 
   const { recipients, subject, body, fileName, fileData } = req.body;
