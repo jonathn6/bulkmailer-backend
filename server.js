fd08a3f69d1b568bf4504ad5367a042ad9790314
jonathan small <jonathn6@icloud.com>
2026-04-10 12:31:49 -0400

    Update server.js
    2025_04_10 12_31_00

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});
+}); 
 
 app.post("/send", async (req, res) => {
   console.log("Incoming request:", req.body);
